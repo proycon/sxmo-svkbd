1076d12924573d73d070288a6f3a142a4a007922
Maarten van Gompel <proycon@anaproy.nl>
2020-07-02 21:15:30 +0200

    made font bold bigger and improved contrast further

--- config.def.h
+++ config.def.h
@@ -2,12 +2,12 @@ static const Bool wmborder = True;
 static int fontsize = 16;
 static int overlay_delay = 1;
 static const char *fonts[] = {
-	"monospace:size=16"
+	"monospace:bold:size=17"
 };
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#58a7c6", "#14313d" },
-	[SchemePress] = { "#ffffff", "#005577" },
+	[SchemeNorm] = { "#ffffff", "#14313d" },
+	[SchemePress] = { "#ffffff", "#000000" },
 	[SchemeHighlight] = { "#58a7c6", "#005577" },
 };
 
