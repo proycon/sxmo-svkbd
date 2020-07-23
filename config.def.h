4fc0c19ff3055da274ddbb7db40efd1a00839642
Miles Alan <m@milesalan.com>
2020-07-23 18:22:39 -0500

    Restore original svkbd colorscheme and bump up to use monospace-18 as font

--- config.def.h
+++ config.def.h
@@ -1,12 +1,11 @@
 static const Bool wmborder = True;
 static int fontsize = 16;
 static const char *fonts[] = {
-	"monospace:size=16"
+	"monospace:size=18"
 };
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#58a7c6", "#14313d" },
-	[SchemePress] = { "#ffffff", "#005577" },
-	[SchemeHighlight] = { "#58a7c6", "#005577" },
+	[SchemeNorm] = { "#000000", "#cccccc" },
+	[SchemePress] = { "#ffffff", "#0000cc" },
+	[SchemeHighlight] = { "#ffffff", "#0000cc" },
 };
-
